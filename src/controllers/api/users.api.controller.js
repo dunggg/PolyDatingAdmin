d44a6c5eac361c8c3346f41e913a4fe402e7177e
QuanPro15 <quannhph11150@fpt.edu.vn>
2021-10-15 23:03:44 +0700

    'update'

--- src/controllers/api/users.api.controller.js
+++ src/controllers/api/users.api.controller.js
@@ -1,5 +1,6 @@
+const multer = require("multer");
 const User = require("../../models/user.shema");
-const { baseJson, validateUser } = require("../../utils/utils");
+const { baseJson, validateUser, uploadMulter } = require("../../utils/utils");
 
 exports.list = async (req, res) => {
     try {
@@ -17,23 +18,31 @@ exports.insert = async (req, res) => {
 
         if (error) return res.json(baseJson(400, error.message));
 
-        const info = {
-            email: value.email,
-            name: value.name,
-            avatars: value.avatars,
-            hobbies: value.hobbies,
-            birthDay: value.birthDay,
-            gender: value.gender,
-            description: value.description,
-            facilities: value.facilities,
-            specialized: value.specialized,
-            course: value.course,
-        };
+        uploadMulter(req, res, (err) => {
+            if (err instanceof multer.MulterError) {
+                if (err.code == 'LIMIT_FILE_SIZE') return res.json('Khong the upload file co kich thuoc lon hon 1 MB');
 
-        User.create(info)
-            .then(() => res.status(201).json(baseJson(201, "Create account successfully")))
-            .catch(err => res.status(400).json(baseJson(400, err.message)));
+                var arr = [];
+                for (let index = 0; index < req.files.length; index++) {
+                    arr.push('src/public/data-image/' + req.files[index].filename);
+                }
+                const info = {
+                    email: value.email,
+                    name: value.name,
+                    avatars: arr,
+                    hobbies: value.hobbies,
+                    birthDay: value.birthDay,
+                    gender: value.gender,
+                    facilities: value.facilities,
+                    specialized: value.specialized,
+                    course: value.course,
+                };
 
+                User.create(info)
+                    .then(() => res.status(201).json(baseJson(201, "Create account successfully")))
+                    .catch(err => res.status(400).json(baseJson(400, err.message)));
+            }
+        });
     } catch (error) {
         res.status(500).json(baseJson(500, error.message));
     }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 const joi = require("joi");
+const multer = require('multer');
 
 /** Base Response */
 const baseJson = (statusCode, message, data = undefined) => {
@@ -9,7 +10,6 @@ const baseJson = (statusCode, message, data = undefined) => {
 const validateUser = joi.object({
   email: joi.string().required(),
   name: joi.string().min(5).max(20).required(),
-  avatars: joi.array().min(2).max(6).required(),
   hobbies: joi.array().min(1).max(5).required(),
   birthDay: joi.string().required(),
   gender: joi.string().required(),
@@ -18,4 +18,22 @@ const validateUser = joi.object({
   course: joi.string().required()
 });
 
-module.exports = { baseJson, validateUser };
+/**  Multer upload image */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'src/public/data-image/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.jpg');
+  }
+});
+
+const uploadMulter = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1 * 1024 * 1024,
+  }
+}).array('avatars', 6);
+
+module.exports = { baseJson, validateUser, uploadMulter };
