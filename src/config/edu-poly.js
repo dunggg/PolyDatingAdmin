ece2d3adb4c14bdc5483dc87d4434ef8a759fb27
QuanPro15 <quannhph11150@fpt.edu.vn>
2021-10-16 23:24:29 +0700

    update

--- src/config/edu-poly.js
+++ src/config/edu-poly.js
@@ -1,6 +1,6 @@
 
 const hobbies = [
-    'Chọn sở thích', "Nghe nhạc", "Xem phim", "Chơi game", "Đá bóng", "Nấu ăn", "Đọc sách", "Bóng đá", "Viết truyện", "69"
+    "Nghe nhạc", "Xem phim", "Chơi game", "Đá bóng", "Nấu ăn", "Đọc sách", "Bóng đá", "Viết truyện", "Thắng Rách"
 ]
 
 const facilities = [
@@ -10,14 +10,14 @@ const facilities = [
 ]
 
 const specialized = [
-    "Thiết kế website", "CNTT - Ứng dụng phần mềm", "Lập trình máy tính - Thiết bị di động", "QTDN - Marketing & Sales",
+    "Chọn ngành học", "Thiết kế website", "CNTT - Ứng dụng phần mềm", "Lập trình máy tính - Thiết bị di động", "QTDN - Marketing & Sales",
     "QHCC - PR & Tổ chức sự kiện", "TMĐT - Digital & Online Marketing", "Thiết kế đồ họa", "Thẩm mỹ làm đẹp",
     "CN kỹ thuật điều khiển và tự động hóa", "CN kỹ thuật điện, điện tử", "Điện công nghiệp", "CN kỹ thuật cơ khí",
     "Hướng dẫn viên du lịch", "Quản trị khách sạn", "Quản trị nhà hàng"
 ]
 
 const course = [
-    'Chọn khóa học', "14", "15", "16", "17"
+    'Chọn khóa học', "14", "15", "16", "17", "18"
 ]
 
 module.exports = { hobbies, facilities, specialized, course }
