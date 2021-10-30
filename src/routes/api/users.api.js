b169ef8a77245f948863be9fed28dda1cd5ba0b8
QuanPro <quannhph11150@fpt.edu.vn>
2021-10-30 21:25:27 +0700

    update

--- src/routes/api/users.api.js
+++ src/routes/api/users.api.js
@@ -5,7 +5,7 @@ const uploadFile = require("../../middlewares/uploadFile");
 
 //Api
 router.get("/list", user.list);
-router.get("/search", user.search);
+router.get("/search/:email", user.search);
 router.post("/insert", uploadFile.uploadFile, user.insert);
 router.post("/delete/:_id", user.delete);
 
