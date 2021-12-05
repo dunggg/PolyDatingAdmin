7a0e70ef0b7cc46cfb776da7437571e3ccbcc13b
QuanPro15 <quannhph11150@fpt.edu.vn>
2021-12-05 17:49:17 +0700

    update

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/public', express.static(__dirname + '/public')); // use bootstrap
 
 // Website
