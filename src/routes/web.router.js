c69168ef929bfd793c6a5333e1a9de2f9ef7d485
dunggg <daotiendung1306@gmail.com>
2021-12-14 02:54:25 +0700

    update

--- src/routes/web.router.js
+++ src/routes/web.router.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const uploadFile = require("../middlewares/uploadFile");
+const uploadFile = require('../middlewares/uploadFile');
 const getTimeZone = require('../middlewares/getTime');
 const { sendMailForgotPassword } = require('../middlewares/sendMail');
 const { pushNotificationUser } = require('../middlewares/notifications');
-const employees = require("../controllers/web/employees");
-const users = require("../controllers/web/users.web");
-
+const employees = require('../controllers/web/employees');
+const users = require('../controllers/web/users.web');
+const { statistical } = require('../controllers/web/statistical.web');
 
 /* Website */
 router.use(getTimeZone);
@@ -15,12 +15,18 @@ router.use(getTimeZone);
 router.get('/', employees.index);
 router.post('/sign-in', employees.signIn);
 router.post('/sign-up', uploadFile, employees.signUp);
-router.post('/forgot-password', employees.forgotPassword, sendMailForgotPassword);
+router.post(
+  '/forgot-password',
+  employees.forgotPassword,
+  sendMailForgotPassword,
+);
 
 //2. Users
 router.get('/users', users.list);
 router.get('/users/:email', users.findOne);
 router.post('/users/block', users.block);
 router.post('/users/unblock', users.unblock);
+
+router.get('/statistical', statistical);
 
 module.exports = router;
