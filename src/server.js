7fcab93ae9447178c86dc11e2e3dfd723a57c892
QuanPro15 <quannhph11150@fpt.edu.vn>
2021-12-16 05:40:03 +0700

    update

--- src/server.js
+++ src/server.js
@@ -20,11 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join(__dirname, 'public'))); // use bootstrap
 
-// Website
-app.use('/', webRouter);
-
 // Api
 app.use('/api', apiRouter);
+
+// Website
+app.use('/', webRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
